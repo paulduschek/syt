79aa0eac22b4429ac0a86803443c9bc216a72774
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2023-05-14 11:43:35 +0200

    updated arduino lcd basics with comments

--- helf/Visuell/Arduino/lcdbasics.ino
+++ helf/Visuell/Arduino/lcdbasics.ino
@@ -6,8 +6,11 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 void setup() {
   pinMode(POTI_PIN, INPUT);
+  // set cols and rows of the lcd in use (in this case 20 columsn, 4 rows)
   lcd.begin(20,4);
+  // set the cursor of the lcd to 0,0
   lcd.home();
+  // clear the lcd screen
   lcd.clear();
   Serial.begin(115200);
 }
@@ -15,6 +18,8 @@ void setup() {
 void loop() {
   int poti = analogRead(POTI_PIN);
 
+  // set the cursor to a specific position (cols, rows)
   lcd.setCursor(6, 3);
+  // print something to the lcd display on the cursor pos currently set
   lcd.print(poti);
 }
